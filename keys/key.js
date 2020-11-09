9a5ac35fab0b7dafebc69cc8f19dd7c0564a83d4
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-10 02:30:18 +0530

    Update key.js

--- keys/key.js
+++ keys/key.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV=="production"){
-    module.exports=require("./prod")
-}else{
-    module.exports=require("./dev")
+module.exports = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback",
+    cookiekey: process.env.COOKIE_KEY,
+    mongoURI: process.env.MONGODB_URI
 }
