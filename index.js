0df0dff397f41247527b0fdc49497a1f7c3c5fa5
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-10 02:42:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV == 'production') {
     app.use(express.static("client/bulid"));
     const path = require("path");
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'bulid', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 app.use(passport.initialize());
