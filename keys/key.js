790e2e28a43b5be2f4b480b730ced5623d357137
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-09 18:50:00 +0530

    yes

--- keys/key.js
+++ keys/key.js
@@ -1,8 +1,7 @@
-module.exports = {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+module.exports={
+    clientID: "679163885433-8pfhh5g8f2vh2lp7ebosgpp3dmhrjkpv.apps.googleusercontent.com",
+    clientSecret: "EDm2guYfZyJ5Z5z1P8ea9v1n",
     callbackURL: "/auth/google/callback",
-    cookiekey: process.env.COOKIE_KEY,
-    mongoURI: process.env.MONGODB_URI
-}
+    cookiekey: "pp123456789212123434"
 
+}
