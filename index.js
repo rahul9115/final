919d30ab81c9e3410ffc3df7d81d254be0d6c7e6
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-10 03:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
 if (process.env.NODE_ENV == 'production') {
     app.use(express.static("client/bulid"));
     const path = require("path");
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
