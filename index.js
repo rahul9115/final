d5805524c1cd64ed752e1c58854b5263eac88086
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-09 16:47:54 +0530

    without client

--- index.js
+++ index.js
@@ -29,13 +29,7 @@ app.use(
 
     })
 );
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static("client/bulid"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'bulid', 'index.html'));
-    })
-}
+
 app.use(passport.initialize());
 app.use(passport.session())
 authroutes(app);
