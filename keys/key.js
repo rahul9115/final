8b18fa4ef6849c73ed47be82e19d3cae4e311413
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-10 02:46:11 +0530

    Update key.js

--- keys/key.js
+++ keys/key.js
@@ -1,1 +1,8 @@
+module.exports = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback",
+    cookiekey: process.env.COOKIE_KEY,
+    mongoURI: process.env.MONGODB_URI
+}
 
