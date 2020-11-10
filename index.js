2d00bbf8ddbab11de18e0b04ae0c2b18941de816
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-10 17:26:29 +0530

    yo

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use(
     })
 );
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client','build','index.html')));
     const path = require("path");
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+        res.sendFile(path.join(__dirname, 'client','build','index.html'));
     })
 }
 app.use(passport.initialize());
