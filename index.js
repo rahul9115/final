36fab119e49b36595ad2ec91b6a0b6350b5ed084
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-11 19:20:26 +0530

    Finally

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ app.use(
 );
 if (process.env.NODE_ENV == 'production') {
   
-    app.use(express.static('client1/build'));
+    
         app.use(express.static('client1/build/index.html'));
 
- 
+        app.use(express.static('client1/build'));
            
         
   
