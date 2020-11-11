079c698f7724ee6a1d83b64124c57ba87f6cfe6c
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-11 19:58:20 +0530

    Finally

--- index.js
+++ index.js
@@ -36,9 +36,7 @@ if (process.env.NODE_ENV == 'production') {
         
 
         app.use(express.static('client1/build'));
-        app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(__dirname,"client1/build/index.html"));
-        })
+        
            
         
   
