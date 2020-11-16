538451c094fce81a93e308505c6870186560afa2
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-17 02:58:43 +0530

    rahul

--- client1/src/components/login.js
+++ client1/src/components/login.js
@@ -4,10 +4,11 @@ import {connect} from "react-redux";
 import css from "./css/login.css";
 import image from "./images/log.png";
 import Body from "./body";
+import axios from "axios";
 var value=false;
 class login extends Component{
     call3(){
-        axios.get("/api/output1").then(res=>{
+        axios.get("/api/output").then(res=>{
             if(res.data){
                 value=true;
             }
