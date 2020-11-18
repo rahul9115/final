c7c1dd4e534ce1850c56cc918a2726d840693bf3
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-18 22:58:05 +0530

    hovering_stopped

--- client1/src/components/paper.js
+++ client1/src/components/paper.js
@@ -45,7 +45,15 @@ class paper extends Component{
        
     }
    
-   
+    async call1(){
+        
+        const res=await axios.get("/api/questions");
+        a3.push(res.data.q);
+        console.log(a3)
+        a.push(res.data.url1)
+        return a3;
+      
+    }
     async call3(){
         
         const res=await axios.get("/api/output1");
