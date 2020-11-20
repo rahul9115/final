a8e2c8c4e42673e6462d5101657730e8965b3f55
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-11-20 15:54:14 +0530

    Update authenticate.js

--- authroutes/authenticate.js
+++ authroutes/authenticate.js
@@ -18,8 +18,8 @@ const mongouri="mongodb+srv://rahul:rahul@cluster0.rpfjy.mongodb.net/<dbname>?re
 const conn =mongoose.createConnection(mongouri);
 var id="";
 const s3=new AWS.S3({
-    accessKeyId:"AKIAIZYMGI7KB3NME3MQ",
-    secretAccessKey:"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"
+    accessKeyId:process.env.AWS_ID,
+    secretAccessKey:process.env.AWS_SECRET
 })
 
 const storage=multer.memoryStorage({
